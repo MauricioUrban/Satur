05b8d3d9232d4a75fed55825e5ba936db428d1e8
Mauricio Urban <mauricio.a.urban@gmail.com>
2025-04-02 23:36:41 -0300

    Configuración inicial con Python y Django, creación de estructura y vista de artículos.

---
+++ sistema/articulo/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def inicio(request):
+    return render(request, 'paginas/inicio.html')
+def nosotros(request):
+    return render(request, 'paginas/nosotros.html')
+def articulos(request):
+    return render(request, 'paginas/articulos/index.html')
+
+
+
+
+
+
+
