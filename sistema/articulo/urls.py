d0babe82098ea8ac526f57ede881bd56fe6adab5
Mauricio Urban <mauricio.a.urban@gmail.com>
2025-04-03 20:11:31 -0300

    Feat: completadas las vistas y la interfaz para creación/edición de artículos

--- sistema/articulo/urls.py
+++ sistema/articulo/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('nosotros', views.nosotros, name='nosotros'),
     path('articulos', views.articulos, name='articulos'),
+    path('articulos/crear', views.crear, name='crear'),
+    path('articulos/editar', views.editar, name='editar'),
 
 ]
 
--- sistema/articulo/views.py
+++ sistema/articulo/views.py
@@ -7,8 +7,17 @@ def inicio(request):
     return render(request, 'paginas/inicio.html')
 def nosotros(request):
     return render(request, 'paginas/nosotros.html')
+
 def articulos(request):
-    return render(request, 'paginas/articulos/index.html')
+    return render(request, 'articulos/index.html')
+def crear(request):
+    return render(request, 'articulos/crear.html')
+def editar(request):
+    return render(request, 'articulos/editar.html')
+
+
+
+
 
 
 
