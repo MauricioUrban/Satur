9265ecc7e41ebe731b7646775c0e027ff3c53b17
Mauricio Arie Urban <mauricio.a.urban@gmail.com>
2025-05-02 15:52:42 -0300

    Feat: mejorar filtros y búsquedas agregando total de pallets

--- sistema/pedido/views.py
+++ sistema/pedido/views.py
@@ -3,6 +3,7 @@ from .models import Pedido
 from .forms import PedidoForm
 from django.shortcuts import render, redirect
 from django.db.models import Q
+from django.db.models import Sum
 
 # Create your views here.
 
@@ -12,26 +13,34 @@ def nosotros(request):
     return render(request, 'paginas/nosotros.html')
 
 def pedidos(request):
-    orden = request.GET.get('orden', 'id') # Orden predeterminado
-    filtro_destinos = request.GET.get('filtro_destinos', '') # Valor del filtro por destino
-    filtro_empresa = request.GET.get('filtro_empresa', '') # Valor del filtro por empresa
+    orden = request.GET.get('orden', 'id')  # Orden predeterminado
+    filtro_destinos = request.GET.getlist('filtro_destinos')  # Obtener lista de destinos seleccionados
+    filtro_empresa = request.GET.get('filtro_empresa', '')  # Valor del filtro por empresa
 
+    # Obtener destinos únicos disponibles en la base de datos
+    destinos_disponibles = Pedido.objects.values_list('destino', flat=True).distinct().order_by('destino')
+
+
+    # Recuperar todos los pedidos y ordenarlos
     pedidos = Pedido.objects.all().order_by(orden)
 
+    # Filtrar por los destinos seleccionados si hay al menos uno marcado
     if filtro_destinos:
         pedidos = pedidos.filter(destino__in=filtro_destinos)
+
     if filtro_empresa:
         pedidos = pedidos.filter(empresa__icontains=filtro_empresa)
 
-    # Obtener destinos únicos para el filtro
-    destinos_disponibles = Pedido.objects.values_list('destino', flat=True).distinct()
+    # Calcular la cantidad total de pallets
+    total_pallets = pedidos.aggregate(Sum('cant_pallets'))['cant_pallets__sum'] or 0   
 
     return render(request, 'pedidos/index.html', {
         'pedidos': pedidos,
         'filtro_destinos': filtro_destinos,
         'filtro_empresa': filtro_empresa,
         'destinos_disponibles': destinos_disponibles,
-        'orden_actual': orden
+        'orden_actual': orden,
+        'total_pallets': total_pallets  # Se envía el total a la plantilla
     })
 
 def crear(request):
